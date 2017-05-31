2449ffd983bce3be7384636a4d7c71ec6a4e3e2c
jose ramirez <joseramirez@joses-MacBook-Pro.local>
2017-05-31 17:37:24 -0400

    set slider to be min:0 -> max:10 and showed how you can connected to the progress bar so that the progress bar advances properly. using the percentage of the slider

--- SlidersAndProgressBars/ViewController.swift
+++ SlidersAndProgressBars/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var progBar: UIProgressView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func didMoveSlider(_ sender: UISlider) {
+        /*1.)we want the progresss bar update according to the slider value*/
+       // progBar.progress = sender.value   //easiest way to do it but not 100% accurate
+        
+    /*what if you cutomize the slider values , so say you have a slider that is min:0 -> max 10*/
+     //we just have to get the percentage that the slider has been moved
+        let percent:Float = sender.value / sender.maximumValue
+        progBar.progress = percent
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
